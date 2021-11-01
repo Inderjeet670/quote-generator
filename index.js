4995cae92f869c0c56342f57db1ca2c62dd06e4f
Inderjeet <jeetinder670@gmail.com>
2021-11-01 15:20:23 -0700

    add a loader while quote is being fetched

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const quoteText = document.querySelector('#quote');
 const authorText = document.querySelector('#author');
 const twitterBtn = document.querySelector('#twitter');
 const newQuoteButton = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
 
 
 let apiQuotes = [];
+//hide loading 
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+//show loading
 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
 // extract a single quote
 function getQuote(){
+    loading();
     const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length) ];
     //IF AUTHOR NAME IS NOT KNOWN
     if(!quote.author){
@@ -23,22 +35,28 @@ function getQuote(){
     }else{
         quoteText.classList.remove('long-quote');
     }
+    //insert quote and hide loader
     quoteText.innerHTML = quote.text;
+    complete();
     
 }
 // Fetch a collection of quotes from API
 
 async function getQuotes(){
+
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
 
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(getQuote())
+        getQuote();
+        
 
     } catch (error) {
         
     }
+    
 }
 
 getQuotes();
